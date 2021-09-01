e19158fb05a61bc158d9da06b860e19e858a7e71
Bryan Cole <cole.bryan.p@gmail.com>
2021-09-01 09:20:33 -0700

    Adding user input

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,5 +1,11 @@
+use std::io;
+
 fn main() {
-    let mars_weight = calculate_weight_on_mars(100.0);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+
+    let mut mars_weight = calculate_weight_on_mars(100.0);
+
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
