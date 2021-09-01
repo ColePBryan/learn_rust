592070f8942520010d02050528be35bda16e62cc
Bryan Cole <cole.bryan.p@gmail.com>
2021-09-01 18:41:27 +0000

    Using user input in weight calculation

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -2,13 +2,16 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
 
-    let mut mars_weight = calculate_weight_on_mars(100.0);
+    println!("Enter your weight(kg):");
+    io::stdin().read_line(&mut input).unwrap();
+    let weight_kg :f32 = input.trim().parse().unwrap();
+
+    let mars_weight = calculate_weight_on_mars(weight_kg);
 
     println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight_on_earth: f32) -> f32 {
-    return (weight_on_earth/9.81) * 3.711;
+    (weight_on_earth/9.81) * 3.711
 }
