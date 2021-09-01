5e7bc9760cdd7d3ffa680bde8db70a411dad8ba9
Bryan Cole <cole.bryan.p@gmail.com>
2021-09-01 07:05:25 -0700

    initial commit of mars calc

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
+}
+
+fn calculate_weight_on_mars(weight_on_earth: f32) -> f32 {
+    return (weight_on_earth/9.81) * 3.711;
 }
