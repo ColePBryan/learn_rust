cb4ab3e73fad891e97dd9903c13b278ea676748b
Bryan Cole <cole.bryan.p@gmail.com>
2021-09-28 19:25:35 -0700

    Adding multithreaded tcp stream handler implementation

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,4 +1,4 @@
-use std::{convert::TryFrom, net::TcpListener, io::Read};
+use std::{convert::TryFrom, net::{TcpListener, TcpStream}, io::Read, thread};
 use crate::http::{Request, Response, StatusCode};
 
 pub struct Server {
@@ -15,35 +15,42 @@ impl Server {
 
         let listener = TcpListener::bind(&self.address).unwrap();
 
-        loop {
-            match listener.accept() {
-                Ok((mut stream, _)) => {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
-                            println!("Parsing request...");
-                            let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => { 
-                                    dbg!(request);
-                                    Response::new(StatusCode::Ok, Some("<h1> IT WORKS STILL!!!! </h1>".to_string()))
-                                },
-                                Err(e) => {
-                                    println!("Failed to parse a request {}", e);
-                                    Response::new(StatusCode::BadRequest, None)
-                                }
-                            };
-                            if let Err(e) = response.send(&mut stream) {
-                                println!("Failed to send response: {}", e);
-                            }
-
-                        }
-                        Err(e) => println!("Failed to read from connection: {}", e)
-                    }
-                },
+        for stream in listener.incoming(){
+            match stream {
+                Ok(stream) => {
+                    thread::spawn(||{
+                        Server::handle_connection(stream);
+                    });
+                }
                 Err(e) => println!("Failed to establish a connection: {}", e)
-                
             }
         }
+
+    }
+
+    pub fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(_) => {
+                println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+                println!("Parsing request...");
+                let response = match Request::try_from(&buffer[..]) {
+                    Ok(request) => { 
+                        dbg!(request);
+                        Response::new(StatusCode::Ok, Some("<h1> IT WORKS STILL!!!! </h1>".to_string()))
+                    },
+                    Err(e) => {
+                        println!("Failed to parse a request {}", e);
+                        Response::new(StatusCode::BadRequest, None)
+                    }
+                };
+                if let Err(e) = response.send(&mut stream) {
+                    println!("Failed to send response: {}", e);
+                }
+
+            }
+            Err(e) => println!("Failed to read from connection: {}", e)
+        }
+            
     }
 }
